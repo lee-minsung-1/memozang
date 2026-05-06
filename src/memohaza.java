0924d7088c974a2ba8188d3c9ffb9257f6329e62
minsung <2012dlalstjd@naver.com>
2026-05-06 14:45:30 +0900

    김범수

--- src/memohaza.java
+++ src/memohaza.java
@@ -3,6 +3,7 @@ public class memohaza {
 
         System.out.println("아 숙취가 너무 심해요 ㅋㅋ");
         System.out.println("나는 여자친구가 생겼으면좋겠어!!! 시발 인생 ㅈ같다~~~ ");
+        System.out.println("그대~ㅜ 오직 그대 만이~ 내~첫사랑~내~끝사랑 지금부터~ 달라질수없는 한가지~ 그대만이 영원한 내 사랑~");
     }
 
 }
